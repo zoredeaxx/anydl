81b5d73aa94d34ad164cbe1867408b8f6bc5dcfd
Code X Mania a.k.a Adarsh Goel <adarsh.goel05@gmail.com>
2021-12-06 17:48:30 +0530

    Update download_stickers.py

--- plugins/download_stickers.py
+++ plugins/download_stickers.py
@@ -28,7 +28,7 @@ from helper_funcs.display_progress import progress_for_pyrogram
 
 @pyrogram.Client.on_message(pyrogram.filters.sticker)
 async def DownloadStickersBot(bot, update):
-    if update.from_user.id not in Config.AUTH_USERS:
+    if update.from_user.id  in Config.BANNED_USERS:
         await bot.delete_messages(
             chat_id=update.chat.id,
             message_ids=update.message_id,
