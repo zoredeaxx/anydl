8a49f35b19b7e99567736846845121bfb9a94635
Code X Mania a.k.a Adarsh Goel <adarsh.goel05@gmail.com>
2021-12-06 17:52:44 +0530

    Update unzip.py

--- plugins/unzip.py
+++ plugins/unzip.py
@@ -30,7 +30,7 @@ from helper_funcs.display_progress import progress_for_pyrogram, humanbytes
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["unzip"]))
 async def unzip(bot, update):
-    if update.from_user.id not in Config.AUTH_USERS:
+    if update.from_user.id  in Config.BANNED_USERS:
         await bot.delete_messages(
             chat_id=update.chat.id,
             message_ids=update.message_id,
