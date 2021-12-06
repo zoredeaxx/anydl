ed3b0ec2f14ac74fd820c26f0319bc100f2e1eef
Code X Mania a.k.a Adarsh Goel <adarsh.goel05@gmail.com>
2021-12-06 17:49:41 +0530

    Update go files.py

--- plugins/go files.py
+++ plugins/go files.py
@@ -36,7 +36,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["go"]))
 async def get_link(bot, update):
-    if update.from_user.id  in Config.AUTH_USERS:
+    if update.from_user.id  in Config.BANNED_USERS:
         await bot.delete_messages(
             chat_id=update.chat.id,
             message_ids=update.message_id,
