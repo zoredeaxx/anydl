a6cb0022f6d9e6c1cdc7e75a71e4f7878324e94f
Code X Mania a.k.a Adarsh Goel <adarsh.goel05@gmail.com>
2021-12-06 17:46:37 +0530

    Update convert_to_video.py

--- plugins/convert_to_video.py
+++ plugins/convert_to_video.py
@@ -36,7 +36,7 @@ from PIL import Image
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["convert2video"]))
 async def convert_to_video(bot, update):
-    if update.from_user.id not in Config.AUTH_USERS:
+    if update.from_user.id  in Config.BANNED_USERS:
         await bot.delete_messages(
             chat_id=update.chat.id,
             message_ids=update.message_id,
