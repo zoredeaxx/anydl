a996b6e2466ba6e85e05021a69a323c06fcfc182
zoredeaxx <87483466+zoredeaxx@users.noreply.github.com>
2022-02-13 18:12:58 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -4,13 +4,13 @@ class Config(object):
     # get a token from https://chatbase.com
     CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5255818430:AAHWiVqgHgHgT7EtDojkvAVULtwtAuJc-CQ")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = int(os.environ.get("APP_ID", "7523379"))
+    API_HASH = os.environ.get("API_HASH", "ce43762f206dc2a2eb115986fbe3b4a2")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "429535048").split())
     # Banned Unwanted Members..
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     # the download location, where the HTTP Server runs
