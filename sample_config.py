6a8fb12a287d9247fb4531a7f30f4e5d1ec99c27
zoredeaxx <87483466+zoredeaxx@users.noreply.github.com>
2022-02-14 21:33:03 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -4,7 +4,7 @@ class Config(object):
     # get a token from https://chatbase.com
     CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5255818430:AAHWiVqgHgHgT7EtDojkvAVULtwtAuJc-CQ")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5032067103:AAF091csf3zyGpRCaTUGCKtVLcOaOncCJIc")
     # The Telegram API things
     APP_ID = int(os.environ.get("APP_ID", "7523379"))
     API_HASH = os.environ.get("API_HASH", "ce43762f206dc2a2eb115986fbe3b4a2")
